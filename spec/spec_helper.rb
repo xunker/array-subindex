f092b96b7365acc489b76360b41c7450e4041a11
Matthew Nielsen <xunker@pyxidis.org>
2014-08-28 10:24:22 -0600

    Don't try to use codeclimate-test-reporter with mri 1.8.7

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+if RUBY_VERSION > "1.8.7"
+  # codeclimate reporter no working with < 1.9.3
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
 
 RSpec.configure do |config|
   config.order = "random"
